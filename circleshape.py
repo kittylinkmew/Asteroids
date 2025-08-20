996f3324c52174dd766f145e3721570962b77712
maryk.maddox@gmail.com <maryk.maddox@gmail.com>
2025-08-19 21:55:15 -0400

    C:Add CircleShape class for sprite collision detecting

---
+++ circleshape.py
@@ -0,0 +1,22 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        #will be overriding with sub-classes
+        pass
+
+    def update(self,dt):
+        #will be overriding with sub-classes
+        pass
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.time import Clock
-
-from constants import *
+from constants import*
+from player import Player
 
 def main():
     pygame.init()
@@ -11,15 +11,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60)/1000
+
+
 
 if __name__ == "__main__":
     main()
